6c6c355566c057eca79ad9e39788b8bb4c955759
RetroCraft <retrocraft314@gmail.com>
2017-10-12 10:56:07 -0400

    Setup

---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+const path = require('path');
+require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, './src/app'),
+
+  entry: {
+    app: './app.jsx',
+  },
+
+  devtool: 'source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: [/node_modules/],
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['es2015', 'react'],
+              plugins: ['transform-object-rest-spread', 'lodash'],
+            },
+          },
+        ],
+      },
+
+      {
+        test: /\.(sass|scss)$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              modules: true,
+              localIdentName: '[name]__[local]___[hash:base64:5]',
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'dist/app/fonts/[name].[ext]',
+            },
+          },
+        ],
+      },
+
+      {
+        test: /\.(png|jpg|jpeg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'dist/app/img/[name].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  output: {
+    path: path.resolve(__dirname, './dist/app'),
+    filename: 'bundle.js',
+  },
+
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: 'The Network of Insanity',
+    }),
+  ],
+
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
