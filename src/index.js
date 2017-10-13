4f5a3acb8ba20ab9a5e0f45206be28af3bdedda7
RetroCraft <retrocraft314@gmail.com>
2017-10-12 21:38:23 -0400

    Change port so no conflict with retrocraft/network-of-insanity

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import path from 'path';
 require('babel-polyfill');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 19847;
 
 app.use(express.static(path.resolve(__dirname, 'app')));
 
