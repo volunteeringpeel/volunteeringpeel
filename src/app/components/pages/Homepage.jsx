f96711233550e49880ef36a89eb4daa17cb11a1a
RetroCraft <retrocraft314@gmail.com>
2017-10-12 20:06:31 -0400

    Exec app message

--- src/app/components/pages/Homepage.jsx
+++ src/app/components/pages/Homepage.jsx
@@ -1,9 +1,54 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Segment, Grid, Header, Button, Image, Icon, Form, Container } from 'semantic-ui-react';
+import {
+  Segment,
+  Grid,
+  Header,
+  Button,
+  Image,
+  Icon,
+  Form,
+  Container,
+  Message,
+  Embed,
+} from 'semantic-ui-react';
 
 export default () => (
   <div>
+    <Container style={{ paddingTop: '2em' }}>
+      <Message>
+        <Grid container doubling columns={2}>
+          <Grid.Row>
+            <Message.Header as="h3" style={{ fontSize: '2em' }}>
+              Apply to be part of the exec team!
+            </Message.Header>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column>
+              <p style={{ fontSize: '1.33em' }}>
+                Volunteering Peel Committee Member applications are out now! If you're a grade 9 or
+                10 student who enjoys volunteering and leading volunteers, meeting new people and
+                getting involved, then apply for the Volunteering Peel Executive team. Applications
+                are due on October 15th!
+              </p>
+            </Grid.Column>
+            <Grid.Column>
+              <Embed
+                id="z8SeIcQ8os8"
+                placeholder="https://img.youtube.com/vi/z8SeIcQ8os8/hqdefault.jpg"
+                source="youtube"
+              />
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <a href="https://drive.google.com/file/d/0B4u62qfSypkPeVBVNWdLYzJVUVU/view">
+              <Button primary>Apply Now</Button>
+            </a>
+          </Grid.Row>
+        </Grid>
+      </Message>
+    </Container>
+
     <Segment style={{ padding: '8em 0em' }} vertical>
       <Grid stackable container verticalAlign="middle">
         <Grid.Row>
