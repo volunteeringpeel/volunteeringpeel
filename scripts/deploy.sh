c8475c3590a46380a70d1c94035b22eda5a43eab
RetroCraft <retrocraft314@gmail.com>
2017-10-23 20:05:51 -0400

    Okay travis how about now?

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
-
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/dist/ build@$DEPLOY_HOST:/var/volunteeringpeel/
-ssh -T build@$DEPLOY_HOST <<-'END'
+rsync -r --delete-after $2/dist/ build@$DEPLOY_HOST:/var/volunteeringpeel/
+ssh -T build@$1 <<-'END'
   sudo chmod -R 774 /var/volunteeringpeel
   sudo service nodejs restart
 END
