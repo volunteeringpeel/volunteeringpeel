05a292bf672ede76e5e7c4be8383bebadae8cbbd
RetroCraft <retrocraft314@gmail.com>
2017-11-29 20:57:48 -0500

    document deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,12 @@
-rsync -r --delete-after --exclude node_modules/ $2/dist/ build@$1:/var/volunteeringpeel/
+# THIS SCRIPT IS FOR TRAVIS CI USE ONLY
+
+# Upload generated files
+rsync -rv -delete-after --exclude=node_modules/ --exclude=passwords.json $2/dist/ build@$1:/var/volunteeringpeel/
+# Upload dependency definitions
 scp $2/package.json build@$1:/var/volunteeringpeel/package.json
-# scp $2/src/api/passwords.js build@$1:/var/volunteeringpeel/passwords.js
 scp $2/yarn.lock build@$1:/var/volunteeringpeel/yarn.lock
 
+# Install dependencies on server and restart
 ssh -T build@$1 <<-'END'
   cd /var/volunteeringpeel
   yarn install --ignore-scripts --production
