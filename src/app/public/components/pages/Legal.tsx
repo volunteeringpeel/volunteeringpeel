16a40b93e8412bdb0f78ebb5d5688c5c02ffada2
RetroCraft <retrocraft314@gmail.com>
2018-03-20 21:05:25 -0400

    Add Waiver of Liability from legacy site
    Closes #35

--- src/app/public/components/pages/Legal.tsx
+++ src/app/public/components/pages/Legal.tsx
@@ -16,7 +16,21 @@ export default class Contact extends React.Component {
         </Segment>
         <Segment vertical>
           <Header as="h2">Waiver of Liability</Header>
-          <p>TODO</p>
+          <p>
+            I recognize and understand that there are risks associated with being a volunteer for
+            Volunteering Peel. I hereby waive and release any and all claims for injuries or damages
+            I have against Volunteering Peel, its executives, committee members, and other
+            volunteers caused by the negligence of any of them arising out of my participation as a
+            volunteer. I understand that I will not receive remuneration for the time or services I
+            provide to Volunteering Peel. I understand that Volunteering Peel is not accountable for
+            any financial losses or theft incurred during our events. I understand that photographs
+            or videos will be taken of me at the events and that I will not receive any compensation
+            for photographs taken of the participation at the event. I take full responsibility
+            towards any and all actions and decisions that I make while volunteering, and accept all
+            consequences that may occur. I hereby willingly consent to release Volunteering Peel,
+            and all of its partners, from all liabilities and waive all claims and demands arising
+            from personal injuries or loss of life incurred while participating at the event.
+          </p>
         </Segment>
       </Container>
     );
