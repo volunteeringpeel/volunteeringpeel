7b0b2ba4ecaf81a80c1043960f0390f732c4ca13
RetroCraft <retrocraft314@gmail.com>
2018-04-21 23:27:58 -0400

    feat(admin/Attendance): Show/hide columns

--- src/app/admin/components/pages/Attendance.tsx
+++ src/app/admin/components/pages/Attendance.tsx
@@ -8,7 +8,7 @@ import * as moment from 'moment';
 import * as React from 'react';
 import 'react-widgets/dist/css/react-widgets.css';
 import * as DateTimePicker from 'react-widgets/lib/DateTimePicker';
-import { Dropdown, DropdownItemProps, Form, Table } from 'semantic-ui-react';
+import { Button, Dropdown, DropdownItemProps, Form, Table } from 'semantic-ui-react';
 
 // App Imports
 import { formatDateForMySQL, timeFormat } from '@app/common/utilities';
@@ -26,6 +26,7 @@ interface AttendanceState {
   activeEntry: number;
   execList: DropdownItemProps[];
   confirmLevels: ConfirmLevel[];
+  hidden: Set<number>;
 }
 
 interface AttendanceEntry {
@@ -65,6 +66,7 @@ export default class Attendance extends React.Component<AttendanceProps, Attenda
       execList: [],
       activeData: [],
       activeEntry: null,
+      hidden: new Set(),
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -181,156 +183,187 @@ export default class Attendance extends React.Component<AttendanceProps, Attenda
       text: level.name,
     }));
 
+    const columns = [
+      'Status',
+      'First Name',
+      'Last Name',
+      'Phone Numbers',
+      'Email',
+      'Start and End',
+      'Hours',
+      'Other Shifts',
+      'Assigned Exec',
+    ];
+
     return (
       <Form onSubmit={this.handleSubmit}>
-        <Dropdown
-          placeholder="Select Shift"
-          fluid
-          search
-          selection
-          options={shifts}
-          value={this.state.activeEntry ? this.state.activeEntry : null}
-          onChange={(e, { value }) =>
-            this.setState({
-              activeEntry: +value,
-              activeData: _.filter(this.state.attendance, ['shift.shift_id', +value]),
-            })
-          }
-        />
+        <Form.Field>
+          <Dropdown
+            placeholder="Select Shift"
+            fluid
+            search
+            selection
+            options={shifts}
+            value={this.state.activeEntry ? this.state.activeEntry : null}
+            onChange={(e, { value }) =>
+              this.setState({
+                activeEntry: +value,
+                activeData: _.filter(this.state.attendance, ['shift.shift_id', +value]),
+              })
+            }
+          />
+        </Form.Field>
         {this.state.activeEntry && (
           <>
+            <Form.Field inline>
+              <label>Columns:</label>
+              <Button.Group
+                buttons={_.map(columns, (column, i) => {
+                  const hidden = this.state.hidden.has(i);
+                  return {
+                    key: i,
+                    content: column,
+                    basic: hidden,
+                    onClick: (e: React.MouseEvent<HTMLButtonElement>) => {
+                      e.preventDefault();
+                      e.stopPropagation();
+                      this.setState(
+                        update(this.state, {
+                          hidden: {
+                            [hidden ? '$remove' : '$add']: [i],
+                          },
+                        }),
+                      );
+                    },
+                  };
+                })}
+              />
+            </Form.Field>
             <Table
               compact
               celled
-              headerRow={[
-                'Status',
-                'First Name',
-                'Last Name',
-                'Phone Numbers',
-                'Email',
-                'Start and End',
-                'Hours',
-                'Other Shifts',
-                'Assigned Exec',
-              ]}
+              headerRow={_.filter(columns, (__, i) => !this.state.hidden.has(i))}
               renderBodyRow={(entry: AttendanceEntry) => {
                 const assignedExec = _.find(this.state.execList, ['user_id', entry.assigned_exec]);
                 return {
                   key: entry.user_shift_id,
-                  cells: [
-                    {
-                      key: 'confirm_level_id',
-                      content: (
-                        <Dropdown
-                          inline
-                          fluid
-                          search
-                          options={statuses}
-                          value={entry.confirm_level_id}
-                          onChange={(e, { value }) =>
-                            this.handleUpdate(entry.user_shift_id, 'confirm_level_id', value)
-                          }
-                        />
-                      ),
-                    },
-                    entry.user.first_name,
-                    entry.user.last_name,
-                    {
-                      key: 'phone_numbers',
-                      content: (
-                        <span>
-                          <a href={`tel:${entry.user.phone_1}`}>{entry.user.phone_1}</a>
-                          <br />
-                          <a href={`tel:${entry.user.phone_2}`}>{entry.user.phone_2}</a>
-                        </span>
-                      ),
-                    },
-                    {
-                      key: 'email',
-                      content: <a href={`mailto:${entry.user.email}`}>{entry.user.email}</a>,
-                    },
-                    {
-                      key: 'time',
-                      content: (
-                        <>
-                          <DateTimePicker
-                            value={new Date(entry.start_time)}
-                            onChange={value =>
-                              this.handleUpdate(
-                                entry.user_shift_id,
-                                'start_time',
-                                value.toISOString(),
-                              )
-                            }
-                          />
-                          <DateTimePicker
-                            value={new Date(entry.end_time)}
-                            onChange={value =>
-                              this.handleUpdate(
-                                entry.user_shift_id,
-                                'end_time',
-                                value.toISOString(),
-                              )
-                            }
-                          />
-                        </>
-                      ),
-                    },
-                    {
-                      key: 'hours',
-                      content: (
-                        <>
-                          {timeFormat(
-                            moment.duration(moment(entry.end_time).diff(entry.start_time)),
-                          )}{' '}
-                          +
-                          <Form.Input
+                  cells: _.filter(
+                    [
+                      {
+                        key: 'confirm_level_id',
+                        content: (
+                          <Dropdown
                             inline
                             fluid
-                            type="text"
-                            size="mini"
-                            pattern="-?[0-9]+(:[0-9]{2})?(:[0-9]{2})?"
-                            value={entry.hours_override}
-                            placeholder="00:00"
-                            onChange={(e, { value }) => {
-                              if (/^-?[0-9]+:?[0-9]{0,2}:?[0-9]{0,2}?$/.test(value)) {
-                                e.currentTarget.setCustomValidity('');
-                                this.handleUpdate(entry.user_shift_id, 'hours_override', value);
-                              } else if (!value) {
-                                // set to '' if empty (because empty doesn't match regex)
-                                this.handleUpdate(entry.user_shift_id, 'hours_override', '');
-                              } else {
-                                e.currentTarget.setCustomValidity('Please type a duration hh:mm');
-                              }
-                            }}
+                            search
+                            options={statuses}
+                            value={entry.confirm_level_id}
+                            onChange={(e, { value }) =>
+                              this.handleUpdate(entry.user_shift_id, 'confirm_level_id', value)
+                            }
                           />
-                          ={' '}
-                          {timeFormat(
-                            moment
-                              .duration(moment(entry.end_time).diff(entry.start_time))
-                              .add(entry.hours_override),
-                          )}
-                        </>
-                      ),
-                    },
-                    entry.other_shifts,
-                    {
-                      key: 'assigned_exec',
-                      content: (
-                        <Dropdown
-                          inline
-                          fluid
-                          search
-                          options={this.state.execList}
-                          value={entry.assigned_exec}
-                          onChange={(e, { value }) =>
-                            this.handleUpdate(entry.user_shift_id, 'assigned_exec', value)
-                          }
-                        />
-                      ),
-                      positive: entry.assigned_exec === this.props.user.user_id,
-                    },
-                  ],
+                        ),
+                      },
+                      entry.user.first_name,
+                      entry.user.last_name,
+                      {
+                        key: 'phone_numbers',
+                        content: (
+                          <span>
+                            <a href={`tel:${entry.user.phone_1}`}>{entry.user.phone_1}</a>
+                            <br />
+                            <a href={`tel:${entry.user.phone_2}`}>{entry.user.phone_2}</a>
+                          </span>
+                        ),
+                      },
+                      {
+                        key: 'email',
+                        content: <a href={`mailto:${entry.user.email}`}>{entry.user.email}</a>,
+                      },
+                      {
+                        key: 'time',
+                        content: (
+                          <>
+                            <DateTimePicker
+                              value={new Date(entry.start_time)}
+                              onChange={value =>
+                                this.handleUpdate(
+                                  entry.user_shift_id,
+                                  'start_time',
+                                  value.toISOString(),
+                                )
+                              }
+                            />
+                            <DateTimePicker
+                              value={new Date(entry.end_time)}
+                              onChange={value =>
+                                this.handleUpdate(
+                                  entry.user_shift_id,
+                                  'end_time',
+                                  value.toISOString(),
+                                )
+                              }
+                            />
+                          </>
+                        ),
+                      },
+                      {
+                        key: 'hours',
+                        content: (
+                          <>
+                            {timeFormat(
+                              moment.duration(moment(entry.end_time).diff(entry.start_time)),
+                            )}{' '}
+                            +
+                            <Form.Input
+                              inline
+                              fluid
+                              type="text"
+                              size="mini"
+                              pattern="-?[0-9]+(:[0-9]{2})?(:[0-9]{2})?"
+                              value={entry.hours_override || ''}
+                              placeholder="00:00"
+                              onChange={(e, { value }) => {
+                                if (/^-?[0-9]+:?[0-9]{0,2}:?[0-9]{0,2}?$/.test(value)) {
+                                  e.currentTarget.setCustomValidity('');
+                                  this.handleUpdate(entry.user_shift_id, 'hours_override', value);
+                                } else if (!value) {
+                                  // set to '' if empty (because empty doesn't match regex)
+                                  this.handleUpdate(entry.user_shift_id, 'hours_override', '');
+                                } else {
+                                  e.currentTarget.setCustomValidity('Please type a duration hh:mm');
+                                }
+                              }}
+                            />
+                            ={' '}
+                            {timeFormat(
+                              moment
+                                .duration(moment(entry.end_time).diff(entry.start_time))
+                                .add(entry.hours_override),
+                            )}
+                          </>
+                        ),
+                      },
+                      entry.other_shifts,
+                      {
+                        key: 'assigned_exec',
+                        content: (
+                          <Dropdown
+                            inline
+                            fluid
+                            search
+                            options={this.state.execList}
+                            value={entry.assigned_exec}
+                            onChange={(e, { value }) =>
+                              this.handleUpdate(entry.user_shift_id, 'assigned_exec', value)
+                            }
+                          />
+                        ),
+                        positive: entry.assigned_exec === this.props.user.user_id,
+                      },
+                    ],
+                    (__, i) => !this.state.hidden.has(i),
+                  ),
                   warning: entry.changed,
                 };
               }}
