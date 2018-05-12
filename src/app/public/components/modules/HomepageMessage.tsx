4006398272d608dca2cb073967c00e44f9b63831
RetroCraft <retrocraft314@gmail.com>
2018-05-12 13:37:41 -0400

    feat(public/Homepage): Extract message box

---
+++ src/app/public/components/modules/HomepageMessage.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import { Button, Container, Embed, Grid, Header, Message } from 'semantic-ui-react';
+
+// PUT ANY MESSAGE BOX THAT YOU WANT TO EXPORT HERE. DO NOT DELETE THE EXAMPLE ONE
+export default (): React.Component => {
+  // EXAMPLE MESSAGE BOX.
+  // return (
+  //   <Container style={{ paddingTop: '1em' }}>
+  //     <Message>
+  //       <Grid container doubling columns={2}>
+  //         <Grid.Row>
+  //           <Grid.Column>
+  //             <Header as="h3">Apply to be part of the exec team!</Header>
+  //             <p>
+  //               Volunteering Peel Committee Member applications are out now! If you're a grade 9 or
+  //               10 student who enjoys volunteering and leading volunteers, meeting new people and
+  //               getting involved, then apply for the Volunteering Peel Executive team. Applications
+  //               are due on October 15th!
+  //             </p>
+  //             <a href="https://drive.google.com/file/d/0B4u62qfSypkPeVBVNWdLYzJVUVU/view">
+  //               <Button primary>Apply Now</Button>
+  //             </a>
+  //           </Grid.Column>
+  //           <Grid.Column>
+  //             <Embed
+  //               id="z8SeIcQ8os8"
+  //               placeholder="https://img.youtube.com/vi/z8SeIcQ8os8/hqdefault.jpg"
+  //               source="youtube"
+  //             />
+  //           </Grid.Column>
+  //         </Grid.Row>
+  //       </Grid>
+  //     </Message>
+  //   </Container>
+  // );
+  return null;
+};
--- src/app/public/components/pages/Homepage.tsx
+++ src/app/public/components/pages/Homepage.tsx
@@ -17,40 +17,14 @@ import {
 } from 'semantic-ui-react';
 
 // Component Imports
+import HomepageMessage from '@app/public/components/modules/HomepageMessage';
 import SubscribeBox from '@app/public/components/modules/SubscribeBox';
 
 export default class Homepage extends React.Component {
   public render() {
     return (
       <div className="large text">
-        <Container style={{ paddingTop: '1em' }}>
-          <Message>
-            <Grid container doubling columns={2}>
-              <Grid.Row>
-                <Grid.Column>
-                  <Header as="h3">Apply to be part of the exec team!</Header>
-                  <p>
-                    Volunteering Peel Committee Member applications are out now! If you're a grade 9
-                    or 10 student who enjoys volunteering and leading volunteers, meeting new people
-                    and getting involved, then apply for the Volunteering Peel Executive team.
-                    Applications are due on October 15th!
-                  </p>
-                  <a href="https://drive.google.com/file/d/0B4u62qfSypkPeVBVNWdLYzJVUVU/view">
-                    <Button primary>Apply Now</Button>
-                  </a>
-                </Grid.Column>
-                <Grid.Column>
-                  <Embed
-                    id="z8SeIcQ8os8"
-                    placeholder="https://img.youtube.com/vi/z8SeIcQ8os8/hqdefault.jpg"
-                    source="youtube"
-                  />
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-          </Message>
-        </Container>
-
+        <HomepageMessage />
         <Segment style={{ padding: '8em 0em' }} vertical>
           <Grid stackable container verticalAlign="middle">
             <Grid.Row>
