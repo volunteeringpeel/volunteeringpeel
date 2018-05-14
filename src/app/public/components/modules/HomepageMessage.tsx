492ec779e1c7bfdbcd83d41489fd63916464731b
RetroCraft <retrocraft314@gmail.com>
2018-05-14 19:41:22 -0400

    feat(HomepageMessage): Site Redesign

--- src/app/public/components/modules/HomepageMessage.tsx
+++ src/app/public/components/modules/HomepageMessage.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 import { Button, Container, Embed, Grid, Header, Message } from 'semantic-ui-react';
 
 // PUT ANY MESSAGE BOX THAT YOU WANT TO EXPORT HERE. DO NOT DELETE THE EXAMPLE ONE
-export default (): React.Component => {
+// tslint:disable-next-line:variable-name
+const HomepageMessage: React.ComponentType = () => {
   // EXAMPLE MESSAGE BOX.
   // return (
   //   <Container style={{ paddingTop: '1em' }}>
@@ -33,5 +35,38 @@ export default (): React.Component => {
   //     </Message>
   //   </Container>
   // );
-  return null;
+  return (
+    <Container style={{ paddingTop: '1em' }}>
+      <Message>
+        <Header as="h3">Site Redesign!</Header>
+        <p>
+          As you can see, the site has been redesigned! The site may still have bugs, so please bear
+          with us as we transition everything over. Just a few things you should know:
+          <ul>
+            <li>
+              You now must create an account to sign up for events. We chose to do this because it
+              will be easier for us to organize information like contact information and attendance.
+            </li>
+            <li>
+              We have updated our Terms and Conditions and Privacy Policy to better reflect how we
+              use your data. You can find it on the <Link to="/about/legal">legal</Link> page.
+            </li>
+            <li>
+              If you do encounter any bugs or errors, take a screenshot and send it to{' '}
+              <a href="mailto:james.ahyong@gmail.com">james.ahyong@gmail.com</a>. Include details on
+              when/how you encountered the error. We'll try and get it fixed as soon as we can.
+            </li>
+          </ul>
+          If you have any feedback whatsoever really,{' '}
+          <a href="mailto:james.ahyong@gmail.com">shoot me an email</a>, and we'll see what we can
+          do.
+        </p>
+        <p>
+          <em>~~james, the guy who made the site</em>
+        </p>
+      </Message>
+    </Container>
+  );
 };
+
+export default HomepageMessage;
